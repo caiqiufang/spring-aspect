6f0aa8430380961fa9affe3a50d3f527717dfd36
qiufang.cai <1358029020@qq.com>
2019-03-10 22:41:51 +0800

    添加Redission 配置，并完成分布式锁配置

--- src/test/java/com/caisebei/aspect/lock/springaspect/SpringAspectApplicationTests.java
+++ src/test/java/com/caisebei/aspect/lock/springaspect/SpringAspectApplicationTests.java
@@ -1,11 +1,15 @@
 package com.caisebei.aspect.lock.springaspect;
 
 import com.caisebei.aspect.lock.springaspect.bean.TestBean;
+import com.caisebei.aspect.lock.springaspect.util.RedissLockUtil;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.redisson.api.RLock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.logging.Logger;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -17,9 +21,15 @@ public class SpringAspectApplicationTests {
 	@Test
 	public void contextLoads() {
 
-		test.testPrint();
-		test.testPrint2();
-
+		RLock sirialKey = RedissLockUtil.lock("sirialKey");
+		System.out.println("****************** :"  + sirialKey.isLocked());
+		try {
+			Thread.sleep(3000L);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		RedissLockUtil.unlock(sirialKey);
+		System.out.println("*******************: " + sirialKey.isLocked());
 	}
 
 }
